6327b9e73bdc2b8d8ce8cb9b27f7e892f13096d1
magoo <mael.gouacide@gmail.com>
2018-12-12 23:43:14 +0100

    Updates

---
+++ precache-manifest.e3324109b68bd4a584dd8309d45f7800.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec60374866f00741b50a",
+    "url": "/static/css/main.858ae8f9.chunk.css"
+  },
+  {
+    "revision": "ec60374866f00741b50a",
+    "url": "/static/js/main.ec603748.chunk.js"
+  },
+  {
+    "revision": "2236606bb076410ffab8",
+    "url": "/static/js/1.2236606b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d39a8533c32e5384a4e4762ea1067916",
+    "url": "/index.html"
+  }
+];
